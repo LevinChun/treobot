0dc6070bfc3f8255cd6beffb28fc5c276a7979ee
LevinChun <96810516+LevinChun@users.noreply.github.com>
2023-03-31 05:20:53 +0700

    Update invite.jsx
    Signed-off-by: LevinChun <96810516+LevinChun@users.noreply.github.com>

--- pages/invite.jsx
+++ pages/invite.jsx
@@ -9,7 +9,7 @@ export default async function invite() {
 }
 
 export async function getServerSideProps({ res }) {
-  res.writeHead(302, { Location: 'https://discord.com/oauth2/authorize?client_id=1004699301982773259&permissions=8&scope=bot%20applications.commands' });
+  res.writeHead(302, { Location: 'https://discord.com/oauth2/authorize?client_id=1004699301982773259&permissions=08&scope=bot%20applications.commands' });
   res.end();
 
   return {
